86048b3baeba930aa0f264c703fee32583987608
Nikolay Valkov (valkovn) <valkovn@coventry.ac.uk>
2018-02-14 12:42:11 +0000

    Update mainMenuLinux.cpp

--- mainMenuLinux.cpp
+++ mainMenuLinux.cpp
@@ -258,30 +258,42 @@ int main()
                                 int inboxMenuHighlight = 0;
                                 
                                 while(1)
-                                {
-                                    for(int i = 0; i < 4; i++)
-                                    {
-                                        if( i == inboxMenuHighlight)
-                                            wattron(inboxMenu, A_STANDOUT);
-                                        mvwprintw(inboxMenu, i + 7, 3, inboxMenuOptions[i].c_str());
-                                        wattroff(inboxMenu, A_STANDOUT);    
-                                    }
-                                    inboxMenuChoice = wgetch(inboxMenu);
-                                    switch(inboxMenuChoice)
-                                    {
-                                        case KEY_UP:
-                                            inboxMenuHighlight--;
-                                            if(inboxMenuHighlight == -1)
-                                                inboxMenuHighlight = 3;
-                                            break;
-                                        case KEY_DOWN:
-                                            inboxMenuHighlight++;
-                                            if(inboxMenuHighlight == 4)
-                                                inboxMenuHighlight = 0;
-                                            break;
-                                        default:
-                                            break;
-                                    }
+        				{
+            				for(int i = 0; i < 4; i++)
+            					{
+                				if( i == inboxMenuHighlight)
+                    					wattron(inboxMenu, A_REVERSE);
+                				mvwprintw(inboxMenu, 5*i + 3, 3, inboxMenuOptions[i].c_str());
+                				wattroff(inboxMenu, A_REVERSE); 
+                				if(inboxMenuOptions[i] != "GO BACK")
+                					{
+                    					string name = "MR. STONE";
+                    					whline(inboxMenu,ACS_HLINE,20);
+                   					 //wattron(inboxMenu,COLOR_PAIR(69));
+                    					whline(inboxMenu,ACS_DIAMOND,i+1);
+                    					//wattroff(inboxMenu,COLOR_PAIR(69));
+                    					//whline(inboxMenu,ACS_DIAMOND,21);
+                    					mvwprintw(inboxMenu,(5*i + 3)+1, 3, "Sender: %s",name.c_str());
+                    					mvwprintw(inboxMenu,(5*i + 3)+2, 3, "Subject: ");
+                    					mvwprintw(inboxMenu,(5*i + 3)+3, 3, "Reward:");
+                					}   
+            					}
+            					inboxMenuChoice = wgetch(inboxMenu);
+            					switch(inboxMenuChoice)
+            					{
+                				case KEY_UP:
+                    					inboxMenuHighlight--;
+                    					if(inboxMenuHighlight == -1)
+                        					inboxMenuHighlight = 3;
+                    					break;
+                				case KEY_DOWN:
+                    					inboxMenuHighlight++;
+                    					if(inboxMenuHighlight == 4)
+                        					inboxMenuHighlight = 0;
+                    					break;
+                				default:
+                    					break;
+            					}
                                     if(inboxMenuChoice == 10)
                                         break;
                                     string inboxMenuInput = inboxMenuOptions[inboxMenuHighlight];
