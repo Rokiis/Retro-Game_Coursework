4adff6edaf655827c7c59ef5a1c3c5a9000a62e5
Daniel Jones (jonesd37) <jonesd37@coventry.ac.uk>
2018-03-12 22:15:52 +0000

    Update mainMenu(GAME).cpp
    integrated leaderboard

--- mainMenu(GAME).cpp
+++ mainMenu(GAME).cpp
@@ -548,7 +548,7 @@ int main()
 
 						wattron(secondScreenWin, COLOR_PAIR(2));
 						keypad(secondScreenWin, true);
-						string secondScreenChoices[5] = { "INBOX", "BANK", "INVENTORY", "LEADERBOARDS", "GO BACK TO MAIN MENU" };
+						string secondScreenChoices[5] = { "INBOX", "BANK", "INVENTORY", "LEADERBOARD", "GO BACK TO MAIN MENU" };
 						int secondScreenChoice;
 						int secondScreenHighlight = 0;
 						while (1)
@@ -763,14 +763,14 @@ int main()
 										//daniel game stuff
 
 
-										
+
 										int danielGameInput;
 										Position * newPosition;
 
 										Level * level;
 
 										level = createLevel(danielGameWin, 0);
-										
+
 
 										DanielGamePlayer = playerSetup(danielGameWin);				//main game initiations
 										int mainEloRating = DanielGamePlayer->eloRating;
@@ -803,6 +803,54 @@ int main()
 
 							}
 						}
+						if (inputSecondScreen == "LEADERBOARD")						//work on
+						{
+							bool leaderboardLoop = false;
+							while (!leaderboardLoop)
+							{
+								wrefresh(secondScreenWin);
+								WINDOW * leaderboard = newwin(20, 70, 1, 1);
+								refresh();
+								wborder(leaderboard, 0, 0, 0, 0, 0, 0, 0, 0);
+								wattron(leaderboard, COLOR_PAIR(1));
+								int leaderboardMaxY, leaderboardMaxX;
+								getmaxyx(leaderboard, leaderboardMaxY, leaderboardMaxX);
+								printMiddle(leaderboard, leaderboardMaxY, leaderboardMaxX, 9, "");
+								wattroff(leaderboard, COLOR_PAIR(1));
+
+								keypad(leaderboard, true);
+								string leaderboardChoices[1] = { "GO BACK" };
+								int leaderboardChoice;
+								int leaderboardHighlight = 0;
+								while (1)
+								{
+									for (int n = 0; n < 1; n++)
+									{
+										if (n == leaderboardHighlight)
+											wattron(leaderboard, A_REVERSE);
+										mvwprintw(leaderboard, n + 17, 2, leaderboardChoices[n].c_str());
+										wattroff(leaderboard, A_REVERSE);
+									}
+									leaderboardChoice = wgetch(leaderboard);
+									switch (leaderboardChoice)
+									{
+									default:
+										break;
+									}
+									if (leaderboardChoice == 10)
+										break;
+								}
+								string inputLeaderboard = leaderboardChoices[leaderboardHighlight];
+								wrefresh(leaderboard);
+								if (inputLeaderboard == "GO BACK")
+								{
+									leaderboardLoop = true;
+									refresh();
+								}
+								wrefresh(leaderboard);
+							}
+						}
+		
 						if (inputSecondScreen == "GO BACK TO MAIN MENU")
 						{
 							characterCreationLoop = true;
